f213eb60dd1c1afb48fd8a9faefaf18e1ed3bad0
safaweb <mahfoudhi000@gmail.com>
2024-07-08 19:38:37 +0100

    1er commit

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -17,7 +17,7 @@
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="{{ url('/') }}" class="flex items-center">
                     <span
-                        class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ config('app.name') }}</span>
+                        class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Ticket System</span>
                 </a>
                 <div class="flex items-center lg:order-2">
                     <a href="{{ route('filament.auth.login') }}"
@@ -42,7 +42,7 @@
                 </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="{{ url('lamp.png') }}">
+                <img src="{{ url('simsoft-n.png') }}">
             </div>
         </div>
     </section>
@@ -51,8 +51,8 @@
     <section class="bg-gray-50 dark:bg-gray-900 dark:bg-gray-800">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 ">
             <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-                <h2 class="mb-4 text-4xl font-bold text-gray-900 dark:text-white">{{ __('Why Use Helpdesk?') }}</h2>
-                <p class="mb-4 font-light">{{ __('Reasons') }}</p>
+                <h2 class="mb-4 text-4xl font-bold text-gray-900 dark:text-white">{{ __('Pourquoi utiliser un SIMSOFT Ticket ?') }}</h2>
+                <p class="mb-4 font-light">Les tickets offrent un support efficace, centralisent la communication, rationalisent les workflows, offrent des options en libre-service, surveillent les performances et améliorent l'expérience client.</p>
             </div>
         </div>
     </section>
@@ -60,7 +60,7 @@
 
     <footer class="p-4 bg-gray-50 sm:p-6 dark:bg-gray-800">
         <div class="mx-auto max-w-screen-xl">
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <!--<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />-->
             <div class="sm:flex sm:items-center sm:justify-between">
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a
                         href="https://ict.ummi.ac.id" class="hover:underline">{{ config('app.name') }}</a>. All Rights
