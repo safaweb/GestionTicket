865ad8f3f674164c2c87147e641963bdc26c69a8
safaweb <mahfoudhi000@gmail.com>
2024-07-15 17:59:04 +0100

    error chaima

--- app/Filament/Resources/TicketResource/Pages/EditTicket.php
+++ app/Filament/Resources/TicketResource/Pages/EditTicket.php
@@ -12,14 +12,6 @@ class EditTicket extends EditRecord
 
     protected function getActions(): array
     {
-
-        Notification::make()
-        ->title('Il y a un nouveau ticket créé')
-        ->actions([
-            Action::make('Voir')
-                ->url(route('filament.resources.tickets.view', $ticket->id)),
-        ])
-        ->sendToDatabase($receiver);
         
         return [
             Actions\ViewAction::make(),
