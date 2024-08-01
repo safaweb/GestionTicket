2d6d57667a85bea198998e517a3656baa04cd5e2
safaweb <mahfoudhi000@gmail.com>
2024-08-01 12:28:25 +0100

    modif chart1

--- app/Filament/Widgets/StatutsDesTicketsChart.php
+++ app/Filament/Widgets/StatutsDesTicketsChart.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Widgets;
 
 use App\Models\StatutDuTicket;
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
 
 class StatutsDesTicketsChart extends ApexChartWidget
@@ -20,7 +23,41 @@ class StatutsDesTicketsChart extends ApexChartWidget
      * @return array */
     protected function getOptions(): array
     {
-        $statutsDesTickets = StatutDuTicket::select('id', 'name')->withCount(['tickets'])->get();
+        $user = Auth::user();
+
+        // Initialize query for StatutDuTicket
+        $query = StatutDuTicket::query();
+
+        // Check the user's role and filter data accordingly
+        if ($user->hasRole('Super Admin')) {
+            // Super Admins can see all tickets
+            $statutsDesTickets = $query->withCount('tickets')->get();
+        /*} elseif ($user->hasRole('Chef Projet')) {
+            // Get the IDs of the projects related to the user
+            $projectIds = $user->projects()->pluck('id')->toArray();
+        
+            // Query to filter tickets based on the projects associated with the user
+            $statutsDesTickets = $query->whereHas('tickets', function ($q) use ($projectIds) {
+                $q->whereIn('project_id', $projectIds);
+            })->withCount(['tickets' => function ($q) use ($projectIds) {
+                $q->whereIn('project_id', $projectIds);
+            }])->get();*/
+        } elseif ($user->hasRole('Employeur')) {
+            // Employeur can only see tickets where they are the responsible_id
+            $statutsDesTickets = $query->whereHas('tickets', function ($q) use ($user) {
+                $q->where('responsible_id', $user->id);
+            })->withCount(['tickets' => function ($q) use ($user) {
+                $q->where('responsible_id', $user->id);
+            }])->get();
+        } else {
+            // Clients or other roles can only see tickets they created (as owner)
+            $statutsDesTickets = $query->whereHas('tickets', function ($q) use ($user) {
+                $q->where('owner_id', $user->id);
+            })->withCount(['tickets' => function ($q) use ($user) {
+                $q->where('owner_id', $user->id);
+            }])->get();
+        }
+
         return [
             'chart' => [
                 'type' => 'pie',
@@ -35,5 +72,20 @@ class StatutsDesTicketsChart extends ApexChartWidget
                 ],
             ],
         ];
+        /*$statutsDesTickets = StatutDuTicket::select('id', 'name')->withCount(['tickets'])->get();
+        return [
+            'chart' => [
+                'type' => 'pie',
+                'height' => 300,
+            ],
+            'series' => $statutsDesTickets->pluck('tickets_count')->toArray(),
+            'labels' => $statutsDesTickets->pluck('name')->toArray(),
+            'legend' => [
+                'labels' => [
+                    'colors' => '#9ca3af',
+                    'fontWeight' => 600,
+                ],
+            ],
+        ];*/
     }
 }
